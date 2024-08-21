baa18dce1e5c96129c0f3379916c97cc9702d87a
gabicordero <corderogabriel6@gmail.com>
2024-08-21 10:23:01 -0300

    add async

--- src/app/confirmation/page.js
+++ src/app/confirmation/page.js
@@ -1,4 +1,13 @@
-export default function ConfirmationPage() {
+import { redirect } from 'next/navigation'
+import { createClient } from '../utils/supabase/server'
+
+export default async function ConfirmationPage() {
+  const supabase = createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  if(error || !data?.user) {
+    redirect('/login')
+  }
   return (
     <p>Te enviamos un mail de confirmación a {data.user.email}</p>
   )
